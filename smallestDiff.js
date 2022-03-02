6ba32d27aeb17cd9c547f1f36596ae7f5300739a
James Hager <james@avantiapplications.com>
2022-03-02 10:46:01 -0700

    Added second method to do cross comparison

--- smallestDiff.js
+++ smallestDiff.js
@@ -17,6 +17,7 @@ Write a function that determines the smallest difference.
 //[20, 3, 40, 17, 78]
 
 function smallestDiff(nums1,nums2) {
+    // compares index by index
     let minVal = Number.MAX_VALUE  // 1.7976931348623157e+308
     for (let i = 0; i < nums1.length; i++) {
         minVal = Math.min(minVal, Math.abs(nums1[i] - nums2[i]))
@@ -24,9 +25,20 @@ function smallestDiff(nums1,nums2) {
     return minVal
 }
 
-const largestNumber = Number.MAX_VALUE
-console.log(largestNumber)
-console.log("largestNumber:",largestNumber)
+function smallestDiff2(nums1,nums2) {
+    // compares across each index of both arrays
+    let minVal = Number.MAX_VALUE  // 1.7976931348623157e+308
+    for (let i = 0; i < nums1.length; i++) {
+        for (let j = 0; j < nums2.length; j++) {
+            minVal = Math.min(minVal, Math.abs(nums1[i] - nums2[j]))
+        }
+    }
+    return minVal
+}
+
+// const largestNumber = Number.MAX_VALUE
+// console.log(largestNumber)
+// console.log("largestNumber:",largestNumber)
 
 function test(func, inp1, inp2, exp) {
     const res = func(inp1, inp2)
@@ -38,3 +50,7 @@ function test(func, inp1, inp2, exp) {
 }
 
 test(smallestDiff,[10, 20, 14, 16, 18],[30, 23, 54, 33, 96],3)
+test(smallestDiff2,[10, 20, 14, 16, 18],[30, 23, 54, 33, 96],3)
+console.log("=== === ===")
+test(smallestDiff,[10, 20, 14, 16, 18],[23, 30, 54, 33, 96],10)
+test(smallestDiff2,[10, 20, 14, 16, 18],[23, 30, 54, 33, 96],3)
